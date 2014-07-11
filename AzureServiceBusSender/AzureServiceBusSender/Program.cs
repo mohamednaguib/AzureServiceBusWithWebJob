fda737748296c7dd09202b7858faa5d7da4f9474
Mohamed Naguib <mohamed.naguib@socialfruits.com>
2014-07-11 21:41:04 +0200

    commit

---
+++ AzureServiceBusSender/AzureServiceBusSender/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureServiceBusSender
+{
+    public class Program
+    {
+        public static string SericeBusConnectionString = "service_bus_connection_string";
+        public static void Main(string[] args)
+        {
+            string queueName = "queue_name";
+            var namespaceManager = NamespaceManager.CreateFromConnectionString(SericeBusConnectionString);
+            if (!namespaceManager.QueueExists(queueName))
+            {
+                namespaceManager.CreateQueue(queueName);
+            }
+            QueueClient client = QueueClient.CreateFromConnectionString(SericeBusConnectionString, queueName);
+            var message = new BrokeredMessage();
+            message.Properties["first_name"] = "Mohamed";
+            message.Properties["last_name"] = "Naguib";
+            client.Send(message);
+        }
+    }
+}
---
+++ ServiceBusWebJob/ServiceBusWebJob/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Azure.Jobs;
+using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceBusWebJob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string queueName = "queue_name";
+            var _servicesBusConn = "service_bus_connection_string";
+            QueueClient client = QueueClient.CreateFromConnectionString(_servicesBusConn, queueName);
+            client.Receive();
+            while (true)
+            {
+                var msg = client.Receive();
+                if (msg != null)
+                {
+                        string fn = msg.Properties["first_name"].ToString();
+                        string ln = msg.Properties["last_name"].ToString();
+                        string name = fn + " " + ln;
+                        Console.WriteLine("Name = " + name);
+                        Trace.TraceInformation("Name = " + name);
+                        msg.Complete();
+                }
+            }
+        }
+    }
+}
