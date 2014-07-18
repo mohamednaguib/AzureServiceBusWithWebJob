70e37173def47c3d64843b2a6e69e6e18ea8850f
Mohamed Naguib <mohamed.naguib@socialfruits.com>
2014-07-18 17:43:43 +0200

    new webjob sdk project

---
+++ ServiceBus/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Threading;
+using Microsoft.Azure.Jobs;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+
+namespace ServiceBus
+{
+    class Program
+    {
+        private const string QueueNamePrefix = "queue_name";
+
+        private static string _servicesBusConnectionString;
+
+        private static NamespaceManager _namespaceManager;
+
+        public static void SBQueueListener(
+            [ServiceBusTrigger(QueueNamePrefix)] BrokeredMessage msg)
+        {
+            string fn = msg.Properties["first_name"].ToString();
+            string ln = msg.Properties["last_name"].ToString();
+            string fullName = fn + ln;
+        }
+        public static void Main()
+        {
+            _servicesBusConnectionString = ConfigurationManager.ConnectionStrings["ServiceBus"].ConnectionString;
+            _namespaceManager = NamespaceManager.CreateFromConnectionString(_servicesBusConnectionString);
+            JobHostConfiguration config = new JobHostConfiguration()
+            {
+                ServiceBusConnectionString = _servicesBusConnectionString,
+            };
+            JobHost host = new JobHost(config);
+            host.RunAndBlock();
+        }
+
+    }
+}
